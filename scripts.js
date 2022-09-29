9b2b7de1aa39de4b769f080ed419397616cdc8b5
jmsd <sanchezdiezma@gmail.com>
2022-09-29 10:31:47 +0200

    first version

---
+++ scripts.js
@@ -0,0 +1,20 @@
+let stars = document.getElementById('stars');
+let moon = document.getElementById('moon');
+let mountainsBehind = document.getElementById('mountains-behind');
+let btn = document.getElementById('btn');
+let text = document.getElementById('text');
+let mountainsFront = document.getElementById('mountains-front');
+let header = document.querySelector('header');
+
+
+window.addEventListener('scroll', function(){
+    let value = window.scrollY;
+    stars.style.left = value * 0.25 + 'px';
+    moon.style.top = value * 1.05 + 'px';
+    mountainsBehind.style.top = value * 0.5 + 'px';
+    mountainsFront.style.top = value * 0.5 + 'px';
+    text.style.marginRight = value * 4 + 'px';
+    text.style.marginTop = value * 1.5 + 'px';
+    btn.style.marginTop = value * 1.5 + 'px';
+    header.style.top = value * 0.5 + 'px';
+})
